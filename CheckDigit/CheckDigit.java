374082dc36852aa1bedf15fc2fa2dd0932f78f45
Neil Reddy <vreddy2018@pingry.org>
2016-10-14 21:47:42 -0400

    Added palindrome functions to the has digit document

--- CheckDigit/CheckDigit.java
+++ CheckDigit/CheckDigit.java
@@ -3,8 +3,38 @@ public class CheckDigit
 	
 	public static void main(String[] args)
 	{
+	System.out.println("Has digit examples");
 	System.out.println(hasDigit("hello"));
 	System.out.println(hasDigit("hello1"));
+	System.out.println();
+	
+	System.out.println("Recursive Is Palindrome with charAt examples");
+	System.out.println(isPalindrome("racecar"));
+	System.out.println(isPalindrome(""));
+	System.out.println(isPalindrome(" "));
+	System.out.println(isPalindrome("no"));
+	System.out.println();
+	
+	System.out.println("Recursive Is Palindrome without charAt examples");
+	System.out.println(isPalindrome1("racecar"));
+	System.out.println(isPalindrome(""));
+	System.out.println(isPalindrome(" "));
+	System.out.println(isPalindrome1("no"));
+	System.out.println();
+	
+	System.out.println("Iterative is palindrome with charAt examples");
+	System.out.println(isPalindrome2("racecar"));
+	System.out.println(isPalindrome(""));
+	System.out.println(isPalindrome(" "));
+	System.out.println(isPalindrome2("no"));
+	System.out.println();
+	
+	System.out.println("Iterative is palindrome without charAt examples");
+	System.out.println(isPalindrome3("racecar"));
+	System.out.println(isPalindrome(""));
+	System.out.println(isPalindrome(" "));
+	System.out.println(isPalindrome3("no"));
+	System.out.println();
 	}
 	
 	
@@ -28,4 +58,118 @@ public class CheckDigit
 	
 	
 	
+	/**
+	*This method takes in a String and returns true if the given string is a palindrome.
+	*This method does so recursively using charAt
+	*@param s - the String to check whether it is a palindrome
+	*@return boolean true - if palindrome, false otherwise
+	*/
+	public static boolean isPalindrome(String s) //recursive with charAt
+	{
+		if (s.length()>=2)
+		{
+			char first = s.charAt(0);
+			char last = s.charAt(s.length()-1);
+			if (first==last)
+			{
+				return isPalindrome(s.substring(1, s.length()-1));
+			}
+			else
+			{
+				return false;
+			}
+		}	
+		else
+		{
+			return true;
+		}
+	}
+
+	/**
+	*This method takes in a String and returns true if the given string is a palindrome.
+	*This method does so recursively without using charAt
+	*@param s - the String to check whether it is a palindrome
+	*@return boolean true - if palindrome, false otherwise
+	*/
+	public static boolean isPalindrome1(String s) //recursive without charAt
+	{
+		if (s.length()>=2)
+		{
+			String first = s.substring(0,1);
+			String last = s.substring(s.length()-1);
+			
+			if (first.equals(last))
+			{
+				return isPalindrome(s.substring(1, s.length()-1));
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return true;
+		}
+	}
+	
+	/**
+	*This method takes in a String and returns true if the given string is a palindrome.
+	*This method does so iteratively using charAt
+	*@param s - the String to check whether it is a palindrome
+	*@return boolean true - if palindrome, false otherwise
+	*/
+	public static boolean isPalindrome2(String s) //iteratively with charAt
+	{
+		int firstCount = 0;
+		int lastCount = s.length()-1;
+		
+		while (lastCount > firstCount)
+		{
+			char first = s.charAt(firstCount);
+			char last = s.charAt(lastCount);
+			
+			if (first == last) 
+			{
+				firstCount ++;
+				lastCount --;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	*This method takes in a String and returns true if the given string is a palindrome.
+	*This method does so iteratively without using charAt
+	*@param s - the String to check whether it is a palindrome
+	*@return boolean true - if palindrome, false otherwise
+	*/
+	public static boolean isPalindrome3(String s) //iteratively without charAt
+	{
+		int firstCount = 0;
+		int lastCount = s.length()-1;
+		
+		while (lastCount > firstCount)
+		{
+
+			String first = s.substring(firstCount,(firstCount+1));
+			String last = s.substring(lastCount, lastCount+1);
+
+			if (first.equals(last))
+			{
+				firstCount ++;
+				lastCount --;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
 }
