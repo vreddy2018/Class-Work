faa632088d51b114e7fe5067a3f11c6f39595478
Neil Reddy <vreddy2018@pingry.org>
2016-11-29 09:01:41 -0500

    Added comments

--- TwoDArrayPractice/TwoDArrayPractice.java
+++ TwoDArrayPractice/TwoDArrayPractice.java
@@ -1,5 +1,11 @@
 public class TwoDArrayPractice
 {
+	/**
+	* Purpose: This method creates a 4x4 array filled with a count
+	* This method works by creating a 4x4 array and then uses 2 For loops to 
+	* put a count into the array. 
+	* @return array - the array with the count 
+	*/
 	public static int[][] createArray()
 	{
 		int [][]array = new int[4][4];
@@ -12,15 +18,22 @@ public class TwoDArrayPractice
 				count ++;
 			}
 		}
-	
 		return array;
 	}
+
+
 
 	public static void main (String[] args)
 	{
 		printArray(createArray());
 	}
 
+
+	/**
+	* Purpose: This method prints a 2d array
+	* This method works by using 2 For loops to 
+	* go through the rows and columns and printing each item. 
+	*/
 	public static void printArray (int [][] array)
 	{
 		for (int row = 0; row<array.length;row++)
